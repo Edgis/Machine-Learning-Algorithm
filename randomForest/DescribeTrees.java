8b10608dbde2315087277a0793d2760187e9e41f
Edgis <1398107093@qq.com>
2017-12-25 22:09:12 +0800

    Add files via upload

---
+++ randomForest/DescribeTrees.java
@@ -0,0 +1,68 @@
+/**
+ * Random-Forest implementation in JAVA
+ * @Author EDGIS
+ * @Contact guoxianwhu@foxmail.com
+ */
+package com.rf.real;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DescribeTrees {
+    //把txt文件作为输入，导入到randomForest中
+    BufferedReader br = null;
+    String path;
+    public DescribeTrees(String path){
+        this.path = path;
+    }
+
+    public ArrayList<int[]> CreateInput(String path){
+        ArrayList<int[]> DataInput = new ArrayList<int[]>();
+        try {
+            String sCurrentLine;
+            br = new BufferedReader(new FileReader(path));
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                ArrayList<Integer> spaceIndex = new ArrayList<Integer>();//空格的index
+                int i;
+                if(sCurrentLine != null){
+                    sCurrentLine = " " + sCurrentLine + " ";
+                    for(i=0; i < sCurrentLine.length(); i++){
+                        if(Character.isWhitespace(sCurrentLine.charAt(i)))
+                            spaceIndex.add(i);
+                    }
+                    int[] DataPoint = new int[spaceIndex.size()-1];
+                    for(i=0; i<spaceIndex.size()-1; i++){
+                        DataPoint[i]=Integer.parseInt(sCurrentLine.substring(spaceIndex.get(i)+1, spaceIndex.get(i+1)));//substring() 左闭右开 [ )
+                    }
+                    /* print DataPoint
+                    for(k=0; k<DataPoint.length; k++){
+                        //System.out.print("-");
+                        System.out.print(DataPoint[k]);
+                        System.out.print(" ");
+
+                    }
+                    **/
+                    DataInput.add(DataPoint);
+                    //for(int t=0;t<DataInput.get(0).length;t++){
+                    //   System.out.print(DataInput.get(0)[t]+",");
+                    // }
+                    //System.out.println("");
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return DataInput;
+    }
+}
---
+++ randomForest/MainRun.java
@@ -0,0 +1,43 @@
+/**
+ * Random-Forest implementation with JAVA
+ * @Author EDGIS
+ * @Contact guoxianwhu@foxmail.com
+ */
+
+package com.rf.real;
+
+import java.util.ArrayList;
+
+public class MainRun {
+    @SuppressWarnings("static-access")
+    public static void main(String args[]){
+
+        String trainPath = "C:\\Users\\kwok\\Desktop\\Iron_rf_data\\Data.txt";
+        String testPath = "C:\\Users\\kwok\\Desktop\\Iron_rf_data\\Test.txt";
+        int numTrees = 100;
+
+        DescribeTrees DT = new DescribeTrees(trainPath);
+        ArrayList<int[]> Train = DT.CreateInput(trainPath);
+
+        DescribeTrees DT2 = new DescribeTrees(testPath);
+        ArrayList<int[]> Test = DT2.CreateInput(testPath);
+        int categ = 0;
+
+        //the num of labels
+        int trainLength = Train.get(0).length;
+        for(int k=0; k<Train.size(); k++){
+            if(Train.get(k)[trainLength-1] < categ)
+                continue;
+            else{
+                categ = Train.get(k)[trainLength-1];
+            }
+        }
+
+        RandomForest Rf = new RandomForest(numTrees, Train, Test);
+        Rf.C = categ;//the num of labels
+        Rf.M = Train.get(0).length-1;//the num of Attr
+        //属性扰动，每次从M个属性中随机选取Ms个属性，Ms = ln(m)/ln(2)
+        Rf.Ms = (int)Math.round(Math.log(Rf.M)/Math.log(2) + 1);//随机选择的属性数量
+        Rf.Start();
+    }
+}
